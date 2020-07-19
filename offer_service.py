fb9714cff338b6d8d039f1b38bb76dcd9844696b
Jakub Vonšovský <jakub.vonsovsky@neuronsw.com>
2020-07-19 23:29:16 +0200

    Type offer_service

--- offer_service.py
+++ offer_service.py
@@ -1,22 +1,23 @@
+from typing import List, Dict
 import requests
 from config import Config
 
 
 class BearerToken:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.access_token = None
 
-    def renew_bearer_token(self):
+    def renew_bearer_token(self) -> None:
         data = requests.post(Config.TOKEN_URL).json()
         self.access_token = data['access_token']
 
-    def get_token(self):
+    def get_token(self) -> str:
         if self.access_token is None:
             self.renew_bearer_token()
         return self.access_token
 
-    def reset_token(self):
+    def reset_token(self) -> None:
         self.access_token = None
 
 
@@ -24,7 +25,7 @@ class OfferService:
     def __init__(self):
         self.bearer_token = BearerToken()
 
-    def register_product(self, id, name, description, retries=1):
+    def register_product(self, id: int, name: str, description: str, retries: int = 1) -> Dict[str, int]:
         headers = {"Bearer": self.bearer_token.get_token()}
         data = {
             'id': id,
@@ -45,7 +46,7 @@ class OfferService:
             else:
                 raise requests.HTTPError("{}: {}".format(data['code'], data['msg']))
 
-    def get_offers(self, id, retries=1):
+    def get_offers(self, id: int, retries: int = 1) -> List[Dict[str, int]]:
         headers = {"Bearer": self.bearer_token.get_token()}
         url = Config.GET_OFFERS_URL.format(id)
         response = requests.get(url, headers=headers)
@@ -63,4 +64,4 @@ class OfferService:
                 raise requests.HTTPError("{}: {}".format(data['code'], data['msg']))
         if response.status_code == 404:
             print("Error not found: {}".format(data['msg']))
-            return {}
+            return []
