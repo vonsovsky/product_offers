8d921302e2ff35d6c629032712d04139af3f3386
Jakub Vonšovský <jakub.vonsovsky@neuronsw.com>
2020-07-19 23:28:47 +0200

    Replace retrieve and delete products with one transaction

--- model.py
+++ model.py
@@ -1,33 +1,39 @@
+"""
+Provides SQL database layer to products and offers
+"""
+
+from typing import List, Dict
 from custom_connection import CustomConnection
 
 
 class DBModel:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._conn = None
 
-    def init_app(self, file_name):
+    def init_app(self, file_name: str) -> None:
         self._conn = CustomConnection(file_name)
 
-    def create_product(self, name, description):
+    def create_product(self, name: str, description: str) -> int:
         with self._conn.cursor() as cursor:
             query = "INSERT INTO products (name, description) VALUES (\"{}\", \"{}\")".format(name, description)
             cursor.execute(query)
             return cursor.lastrowid
 
-    def retrieve_products(self):
+    def retrieve_products(self) -> List[Dict[str, str]]:
         with self._conn.cursor() as cursor:
-            query = "SELECT name, description FROM products"
+            query = "SELECT id, name, description FROM products"
             cursor.execute(query)
-            return cursor.fetchall()
+            products = [{'id': row[0], 'name': row[1], 'description': row[2]} for row in cursor.fetchall()]
+            return products
 
-    def update_product(self, id, name, description):
+    # TODO batching if we have a lot of products
+    def insert_offers(self, offer_map: Dict[int, List[Dict[str, int]]]) -> None:
         with self._conn.cursor() as cursor:
-            query = "UPDATE products SET name = \"{}\", description = \"{}\" WHERE id = {}"\
-                .format(name, description, id)
-            cursor.execute(query)
-
-    def delete_product(self, id):
-        with self._conn.cursor() as cursor:
-            query = "DELETE FROM products WHERE id = {}".format(id)
-            cursor.execute(query)
+            for product_id, offers in offer_map.items():
+                query = "DELETE FROM offers WHERE product = {}".format(product_id)
+                cursor.execute(query)
+                for offer in offers:
+                    query = "INSERT INTO offers (product, price, items_in_stock) VALUES ({}, {}, {})"\
+                        .format(product_id, offer['price'], offer['items_in_stock'])
+                    cursor.execute(query)
